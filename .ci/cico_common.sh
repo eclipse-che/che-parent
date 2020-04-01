f2b45d5dce5d52767d0cb79cd2e77a44745f6e95
Mykhailo Kuznietsov <mkuznets@redhat.com>
2020-04-01 12:01:37 +0300

    fixup! fixup! fixup! fixup! fixup! Add Nexus configuration

--- .ci/cico_common.sh
+++ .ci/cico_common.sh
@@ -68,10 +68,9 @@ build_and_deploy_artifacts() {
         echo 'Build Success!'
         echo 'Going to deploy artifacts'
     if [[ $(getCurrentVersion) == "*-SNAPSHOT" ]]; then 
-        scl enable rh-maven33 "mvn clean deploy -Pcodenvy-release -DcreateChecksum=true  -Dgpg.passphrase=$CHE_OSS_SONATYPE_PASSPHRASE"
+        scl enable rh-maven33 "mvn clean deploy -DskipStaging=true -Pcodenvy-release -DcreateChecksum=true  -Dgpg.passphrase=$CHE_OSS_SONATYPE_PASSPHRASE"
     else
-        scl enable rh-maven33 "mvn clean deploy -DskipRemoteStaging=true -U -B -Pcodenvy-release -DcreateChecksum=true  -Dgpg.passphrase=$CHE_OSS_SONATYPE_PASSPHRASE -DstagingDescription=\"[$ ${pomVersion} :: deploy to local\"" 
-        scl enable rh-maven33 "mvn nexus-staging:deploy-staged -DstagingDescription=\"[ ${pomVersion} :: deploy to stage + close\""
+        scl enable rh-maven33 "mvn clean deploy -Pcodenvy-release -DcreateChecksum=true  -Dgpg.passphrase=$CHE_OSS_SONATYPE_PASSPHRASE"
     fi
 
     else
